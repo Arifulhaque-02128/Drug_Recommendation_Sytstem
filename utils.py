5cabdd0a5d47cfbcfdd2634b316dfe4d5fc44249
Md. Ariful Haque <67544093+Arifulhaque-02128@users.noreply.github.com>
2024-10-29 01:45:55 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ Created on Tue Oct 29 00:03:23 2024
 import pandas as pd
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('C:/Users/User/Desktop/ML_Projects/Classify_patient_condition_using_drug_reviews/patient_condition_prediction/drugs_data.csv')
+df = pd.read_csv('drugs_data.csv')
 
 def top_drugs_extractor(condition):
     df_top = df[(df['rating'] >= 9) & (df['usefulCount'] >= 100)].sort_values(by=['rating', 'usefulCount'], ascending=[False, False])
