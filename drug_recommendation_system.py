e20b59763dbb6e75c84706d1ee0d7db946b1e448
Md. Ariful Haque <67544093+Arifulhaque-02128@users.noreply.github.com>
2024-10-29 00:59:00 +0600

    Add files via upload

---
+++ drug_recommendation_system.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# import numpy as np
+import pickle
+import streamlit as slit
+#from streamlit_option_menu import option_menu
+#import pandas as pd
+from utils import top_drugs_extractor
+
+
+
+
+#loading the saved model
+patient_condition_model = pickle.load(open('patient_condition_model.sav', 'rb'))
+bigram_tfidf_vectorizer = pickle.load(open('bigram_tfidf_vect.sav', 'rb'))
+
+
+
+def patient_condition_prediction (review) :
+    vectorizer = bigram_tfidf_vectorizer.transform(review)
+    pred = patient_condition_model.predict(vectorizer)
+    
+    return pred
+
+
+
+
+slit.markdown(
+"<h1 style='text-align: center; color: #00c8ff;'>Patient Condition and Drug Recommendation</h1>",
+unsafe_allow_html=True)
+
+drug_review = slit.text_area('Enter Medical Issue for getting Drug Recommendation')
+
+#code for prediction
+patient_condition = ''
+
+# creating a button for prediction
+
+if slit.button('Predict') : 
+    patient_condition = patient_condition_prediction([drug_review])
+    
+    slit.markdown(
+    "<h5>Predicted Medical Condition : </h5>",
+    unsafe_allow_html=True)
+    slit.success(f"{patient_condition[0]}")
+    
+    
+    slit.markdown(
+    "<h5>Top 3 Recommended Drugs : </h5>",
+    unsafe_allow_html=True)
+    for drug in top_drugs_extractor(patient_condition[0]) : 
+        slit.success(f"{drug}")
+    
+        
+        
+    
+        
